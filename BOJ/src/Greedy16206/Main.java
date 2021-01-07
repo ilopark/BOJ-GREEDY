f3466b7f224026f83dd6a0dc9c081a74ecef21fc
ilopark <park3650@naver.com>
2021-01-07 20:10:49 +0900

    16206번 롤케이크

---
+++ BOJ/src/Greedy16206/Main.java
@@ -0,0 +1,32 @@
+package Greedy16206;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int m=scan.nextInt();
+		int count=0;
+		int sum=0;
+		int []arr=new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i]=scan.nextInt();			
+		}
+		for(int i=0; i<n; i++) {
+			if(arr[i]==10) {
+				sum++;
+			}else if(arr[i]>10){
+				while(arr[i]>10&&count<m) {
+					arr[i]-=10;
+					count++;
+					sum++;
+					if(arr[i]==10) {
+						sum++;
+					}
+				}
+			}
+		}
+		System.out.println(sum);
+		
+	}
+}
